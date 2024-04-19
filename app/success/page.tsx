0e19834cabce337299185ffd7c732e15a7b71d06
Tommy Liu <tommyjqliu@outlook.com>
2024-04-19 16:18:44 +0800

    sfskdfjsldkfj

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -2,6 +2,6 @@ import { auth } from "@/lib/auth";
 
 export default async function page() {
     const session = await auth();
-
+    console.log('tests')
     return <div>Login Success: {session?.user?.name}</div>
 }
