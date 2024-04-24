e0cd4584bb9e21342473744700dbca9dbeeb7659
Tommy Liu <tommyjqliu@outlook.com>
2024-04-24 17:46:01 +0800

    feat: user model improve

--- prisma/migrations/20240424094222_problem_and_assignment/migration.sql
+++ prisma/migrations/20240424094222_problem_and_assignment/migration.sql
@@ -1,18 +1,32 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NULL,
+    `username` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ExternalAccount` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `provider` ENUM('MICROSOFT', 'GOOGLE', 'GITHUB') NOT NULL,
+    `providerId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `ExternalAccount_provider_userId_key`(`provider`, `userId`),
+    UNIQUE INDEX `ExternalAccount_provider_providerId_key`(`provider`, `providerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Permission` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `User` (
-    `email` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`email`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -28,17 +42,25 @@ CREATE TABLE `UserGroup` (
 CREATE TABLE `Assignment` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `publishDate` DATETIME(3) NOT NULL,
-    `dueDate` DATETIME(3) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `publishDate` DATETIME(3) NULL,
+    `dueDate` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `AssignmentRoleDescription` (
+    `role` ENUM('STUDENT', 'TUTOR', 'TEACHER') NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`role`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `UsersOnAssignments` (
     `assignmentId` INTEGER NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
     `roles` JSON NOT NULL,
 
     PRIMARY KEY (`assignmentId`, `userId`)
@@ -80,17 +102,20 @@ CREATE TABLE `_PermissionToUserGroup` (
 -- CreateTable
 CREATE TABLE `_PermissionToUser` (
     `A` INTEGER NOT NULL,
-    `B` VARCHAR(191) NOT NULL,
+    `B` INTEGER NOT NULL,
 
     UNIQUE INDEX `_PermissionToUser_AB_unique`(`A`, `B`),
     INDEX `_PermissionToUser_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
+ALTER TABLE `ExternalAccount` ADD CONSTRAINT `ExternalAccount_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `UsersOnAssignments` ADD CONSTRAINT `UsersOnAssignments_assignmentId_fkey` FOREIGN KEY (`assignmentId`) REFERENCES `Assignment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `UsersOnAssignments` ADD CONSTRAINT `UsersOnAssignments_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `UsersOnAssignments` ADD CONSTRAINT `UsersOnAssignments_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `ProblemsOnAssignments` ADD CONSTRAINT `ProblemsOnAssignments_problemId_fkey` FOREIGN KEY (`problemId`) REFERENCES `Problem`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -108,4 +133,4 @@ ALTER TABLE `_PermissionToUserGroup` ADD CONSTRAINT `_PermissionToUserGroup_B_fk
 ALTER TABLE `_PermissionToUser` ADD CONSTRAINT `_PermissionToUser_A_fkey` FOREIGN KEY (`A`) REFERENCES `Permission`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_PermissionToUser` ADD CONSTRAINT `_PermissionToUser_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `_PermissionToUser` ADD CONSTRAINT `_PermissionToUser_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
