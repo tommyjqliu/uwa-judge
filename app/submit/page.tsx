bcce2feb7ff36365ced036a9368ece9b8dccef6a
Seb-Matthews <seb.matthews@outlook.com>
2024-04-26 14:44:56 +0800

    test--blank-pages
    Set up initial structure to test submission

---
+++ app/submit/page.tsx
@@ -0,0 +1,40 @@
+// databse client for domjudge database
+import { PrismaClient as DOMjudgeClient } from '@/lib/domjudge-db-client';
+// databse client for uwajudge database
+import { PrismaClient as UWAjudgeClient }  from '@prisma/client';
+
+export default function Page() {
+
+
+
+    /* const [files, setFiles] = useState<FileList | null>(null);
+    function submitForm(e: MouseEvent) {
+        e.preventDefault();
+
+        const formData = new FormData();
+        
+        for (let i = 0; i < (files?.length || 0); i++) {
+            formData.append("files", files?.[i]!);
+        }
+
+        fetch("/api/problems", {
+            method: 'POST',
+            body: formData,
+        })
+    }
+
+    return <form id='form'>
+        <div>
+            <label htmlFor='files'>Select files</label>
+            <input id='files' type="file" multiple onChange={(e) => setFiles(e.target.files)} />
+        </div>
+        <button type='submit' onClick={(e) => submitForm(e)}>Upload</button>
+    </form> */
+
+
+
+
+    return <div>
+        <h1>Test Submit Page2</h1>
+    </div>
+}
