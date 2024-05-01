7faa7d363055c821f18bcd23c18bf39c7aed9bfc
Seb-Matthews <seb.matthews@outlook.com>
2024-05-01 10:56:20 +0800

    fix--domjudge submit syntax errors
    - Added test problem "p1.zip"
    - fixed an error with "language" being wrong
    - fixed an syntax error with the error_handler on api/submission

--- app/api/submissions/route.ts
+++ app/api/submissions/route.ts
@@ -1,6 +1,7 @@
 import { CONTEST_CID } from "@/lib/constant"    // Unique contest IDs aren't used in our implementation.
 import { SubmissionsApi, djConfig } from "@/lib/domjudge-api-client"
 import { PrismaClient as DOMjudgeClient } from '@/lib/domjudge-db-client';
+import { domjudgeDB, uwajudgeDB } from '@/lib/database-client';
 import { isFile } from "@/lib/file"
 import errorHandler from '@/lib/error-handler';
 
@@ -10,18 +11,17 @@ const submissionsApi = new SubmissionsApi(djConfig)
 //    request: Request,
 //) 
 export const POST = errorHandler(async function (request: Request) {
-    /* const formData = await request.formData()   // get data from user input
+    const formData = await request.formData()   // get data from user input
     const problemId = String(formData.get('problemId')) ?? ''
     const language = String(formData.get('language')) ?? ''
     const entryPoint = String(formData.get('entryPoint')) ?? ''
     const files = formData.getAll('files[]').filter(isFile)
     const res = await submissionsApi.postV4AppApiSubmissionAddsubmissionForm(problemId, language, files, entryPoint, String(CONTEST_CID))
+        /* .catch(function (error) {
+            console.log(error.toJSON());
+        }); */
+    
     return new Response(JSON.stringify(res.data), {
-        status: 200,
-    }) */
-
-
-    return new Response(JSON.stringify("test"), {
-        status: 200,
+        status: 200
     })
-}
+})
--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -8,13 +8,13 @@ import { PrismaClient as UWAjudgeClient }  from '@prisma/client';
 //import { auth } from "@/lib/auth";
 import { MouseEvent, useState } from "react";
 
+import Link from "next/link";
+
 export default function Page() {
 
     const [files, setFiles] = useState<FileList | null>(null);
     function submitForm(e: MouseEvent) {
         e.preventDefault();
-
-
 
         const formData = new FormData();
 
@@ -22,10 +22,10 @@ export default function Page() {
         var problemId:string = '1';  // Must be string for the form
         formData.set("problemId", problemId)
 
-        var language:string = 'Python'
+        var language:string = 'python3'
         formData.set("language", language)
 
-        var entryPoint:string = 'app/submit'
+        var entryPoint:string = ''
         formData.set("entryPoint", entryPoint)
 
         // Add files to form
@@ -42,28 +42,31 @@ export default function Page() {
                 /* if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 } */
-                if(response.body() != null){
+                if(response != null){
                     // Display the result if not NULL
                     //document.getElementById('submitResponse')!.innerHTML = response.body(); // '!' asserts that the "submitResponse" tag will never be NULL
-                    document.getElementById('submitResponse')!.value = response.body(); // '!' asserts that the "submitResponse" tag will never be NULL
+                    document.getElementById('submitResponse').value = response.body(); // '!' asserts that the "submitResponse" tag will never be NULL
                 }
             })
 
     }
 
 
-    return <form id='form'>
-    <div>
-        <h1>Test Upload Submit Page</h1>
+    return <div>
+        <Link href="/..">Back to Main</Link>
+        <form id='form'>
+            <div>
+                <h1>Test Upload Submit Page</h1>
+            </div>
+            <div>
+                <label htmlFor='files'>Select files</label>
+                <input id='files' type="file" multiple onChange={(e) => setFiles(e.target.files)} />
+            </div>
+            <button type='submit' onClick={(e) => submitForm(e)}>Upload</button>
+            <div>
+                <h3>Response:</h3>
+                <textarea id="submitResponse" rows="6" cols="30"> </textarea>
+            </div>
+        </form>
     </div>
-    <div>
-        <label htmlFor='files'>Select files</label>
-        <input id='files' type="file" multiple onChange={(e) => setFiles(e.target.files)} />
-    </div>
-    <button type='submit' onClick={(e) => submitForm(e)}>Upload</button>
-    <div>
-        <h3>Response:</h3>
-        <textarea id="submitResponse" rows="6" cols="30"> </textarea>
-    </div>
-    </form>
 }
