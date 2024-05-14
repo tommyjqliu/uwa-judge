135f50389413fd3220dc765562e28ddfbfc1645d
Zhiyang <958932166@qq.com>
2024-05-14 21:44:48 +0800

    assignments/route-ts, assignments/{assignmentId}/route-ts  usable

--- app/api/assignments/route.ts
+++ app/api/assignments/route.ts
@@ -12,9 +12,9 @@ export const assignmentSchema = zfd.formData({
   description: z.string().optional(),
   publishDate: z.coerce.date().optional(),
   dueDate: z.coerce.date().optional(),
-  students: zfd.repeatable(z.coerce.number().array().default([])),
-  tutors:zfd.repeatable(z.coerce.number().array().default([])),
-  admins:zfd.repeatable(z.coerce.number().array().default([])),
+  students: zfd.repeatable(z.coerce.number().array().default([])).optional(),
+  tutors:zfd.repeatable(z.coerce.number().array().default([])).optional(),
+  admins:zfd.repeatable(z.coerce.number().array().default([])).optional(),
   problems: zfd.repeatable(z.array(zfd.file())), // repearable is nessary for parsing signle file
 });
 
