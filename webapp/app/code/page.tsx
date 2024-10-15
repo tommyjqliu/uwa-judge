aec4db145f0448590b6b126b3efbe30d7fec6b34
Tommy Liu <tommyjqliu@outlook.com>
2024-10-15 17:09:48 +0800

    fix: fix judge result

--- webapp/app/code/page.tsx
+++ webapp/app/code/page.tsx
@@ -7,51 +7,51 @@ import { z } from "zod";
 import ProblemSolver from "./problem-solver";
 
 interface SearchParams {
-    problemId?: string; // assignment problem id
-    problemVersionId?: string; // problem version id
+	problemId?: string; // assignment problem id
+	problemVersionId?: string; // problem version id
 }
 
 export default async function Page({
-    searchParams,
+	searchParams,
 }: {
-    searchParams?: SearchParams;
+	searchParams?: SearchParams;
 }) {
-    const { problemId, problemVersionId } = z
-        .object({
-            problemId: stringToInt.optional(),
-            problemVersionId: stringToInt.optional(),
-        })
-        .parse(searchParams);
+	const { problemId, problemVersionId } = z
+		.object({
+			problemId: stringToInt.optional(),
+			problemVersionId: stringToInt.optional(),
+		})
+		.parse(searchParams);
 
-    let assignment: Assignment | null = null;
-    let problemVersion: ProblemVersion | null = null;
-    if (problemVersionId !== undefined) {
-        problemVersion = await uwajudgeDB.problemVersion.findUnique({
-            where: { id: problemVersionId },
-        });
-    } else if (problemId !== undefined) {
-        assignment = await uwajudgeDB.assignment.findUnique({
-            where: { id: problemId },
-        });
-        const problem = await uwajudgeDB.problem.findUnique({
-            where: { id: problemId },
-            include: { problemVersion: true },
-        });
-        problemVersion = problem?.problemVersion ?? null;
-    }
+	let assignment: Assignment | null = null;
+	let problemVersion: ProblemVersion | null = null;
+	if (problemVersionId !== undefined) {
+		problemVersion = await uwajudgeDB.problemVersion.findUnique({
+			where: { id: problemVersionId },
+		});
+	} else if (problemId !== undefined) {
+		assignment = await uwajudgeDB.assignment.findUnique({
+			where: { id: problemId },
+		});
+		const problem = await uwajudgeDB.problem.findUnique({
+			where: { id: problemId },
+			include: { problemVersion: true },
+		});
+		problemVersion = problem?.problemVersion ?? null;
+	}
 
-    return (
-        <ResizablePanelGroup direction="horizontal" className="flex-grow">
-            {problemVersion && <>
-                <ResizablePanel defaultSize={50} id="statement">
-                    <div className="flex flex-col items-center justify-center h-full">
-                        <div className="font-semibold m-4">{problemVersion.name}</div>
-                        <PdfReader url={`api/problem-versions/${problemVersion.id}/pdf-statement`}  className="border-2 border-gray-300"/>
-                    </div>
-                </ResizablePanel>
-                <ResizableHandle withHandle/>
-            </>}
-            <ProblemSolver problemVersionId={problemVersionId} problemId={problemId} />
-        </ResizablePanelGroup>
-    )
+	return (
+		<ResizablePanelGroup direction="horizontal" className="flex-grow">
+			{problemVersion && <>
+				<ResizablePanel defaultSize={50} id="statement">
+					<div className="flex flex-col items-center justify-center h-full">
+						<div className="font-semibold m-4">{problemVersion.name}</div>
+						<PdfReader url={`api/problem-versions/${problemVersion.id}/pdf-statement`} className="border-2 border-gray-300" />
+					</div>
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+			</>}
+			<ProblemSolver problemVersionId={problemVersionId} problemId={problemId} />
+		</ResizablePanelGroup>
+	)
 }
