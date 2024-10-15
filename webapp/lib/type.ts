9a2af50893a2c529f70dc8efca2086162e0329ea
Tommy Liu <tommyjqliu@outlook.com>
2024-10-15 17:28:48 +0800

    fix: typing

--- webapp/lib/type.ts
+++ webapp/lib/type.ts
@@ -15,3 +15,12 @@ export type Merge<A, B> = {
       : K
     : K]: K extends keyof B ? B[K] : A[K];
 };
+
+type UndefinedProperties<T> = {
+  [P in keyof T]-?: undefined extends T[P] ? P : never
+}[keyof T]
+/**
+ * Convert undefined able properties of T to optional
+ */
+export type ToOptional<T> = Partial<Pick<T, UndefinedProperties<T>>> & Pick<T, Exclude<keyof T, UndefinedProperties<T>>>
+
